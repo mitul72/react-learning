be6a43aed7931c0f9809c1f90104a386bdd28d11
mitul72 <mituldhawan2@gmail.com>
2023-05-06 14:28:34 -0600

    added a navbar and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-
-let name = 'Harry';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container">
-      <h1>Hello {name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ex a suscipit accusantium consectetur modi ut fugit quam commodi minus placeat atque perspiciatis cumque, eos quis. Fugiat quia molestias dolore voluptatum rem quisquam hic!</p>
-    </div>
+    <Navbar title="TextUtils" aboutText="About TextUtils" />
+    {/* <Navbar/> */}
     </>
   );
 }
