c60fc1e477a03461abeb962051479b6c954a1aee
mitul72 <mituldhawan2@gmail.com>
2023-05-06 19:48:31 -0600

    adds dark mode and textform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import {useState} from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [modeText, setModeText] = useState("Enable Dark Mode");
+  const [modeTextColor, setmodeTextColor] = useState("dark")
+  // const [modeColor, setModeColor] = useState("black"); 
+ 
+
+  const toggleMode = () => {
+    if(mode === "light"){
+    setMode("dark");
+    setModeText("Enable Light Mode")
+    setmodeTextColor("light")
+    document.body.style.backgroundColor = 'grey';
+    }
+    else {
+      setMode('light');
+      setModeText('Enable Dark Mode')
+      setmodeTextColor("dark")
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About TextUtils" />
-    {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} modeText={modeText} modeTextColor={modeTextColor} />
+      <div className="container my-4">
+        <TextForm helpText="hello world" heading="New Form" mode={mode} />
+        {/* <About/> */}
+
+      </div>
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
     </>
   );
 }
